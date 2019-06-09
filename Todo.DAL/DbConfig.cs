f5abb18abf41a20d721a3082ee64e4fbfa3e296e
Cheranga Hatangala <cheranga@gmail.com>
2019-06-09 12:44:31 +1000

    Adding local.settings.json and removing the unnecessary factory class.

--- Todo.DAL/DbConfig.cs
+++ Todo.DAL/DbConfig.cs
@@ -2,17 +2,6 @@
 
 namespace Todo.DAL
 {
-    public class DatabaseConfigFactory
-    {
-        public static DbConfig GetConfig()
-        {
-            return new DbConfig
-            {
-                ConnectionString = Environment.GetEnvironmentVariable("DatabaseConnection")
-            };
-        }
-    }
-
     public class DbConfig
     {
         public string ConnectionString { get; set; }
